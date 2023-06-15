2c5faf2e469009ee1b0ad005f69208e0bb3cea2c
MohamedElgamal19 <melgaml41@gmail.com>
2023-06-15 16:21:34 -0700

    alx

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * description: print a qoute using write function
+ *    ssize_t write(int fd, const void *buf, size_t count);
+ *
+ * Return: 1 (error).
+*/
