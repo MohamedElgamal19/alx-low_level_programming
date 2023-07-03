1c285a5a19e8d4adba653a5194bae30357e1956a
MohamedElgamal19 <melgaml41@gmail.com>
2023-07-03 15:24:42 -0700

    a

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -1,3 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+#endif
+root@ab742508554d:/alx-low_level_programming/0x07-pointers_arrays_strings# cat _putchar.c
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
+root@ab742508554d:/alx-low_level_programming/0x07-pointers_arrays_strings# cat 0-main.c 
 #include "main.h"
 #include <stdio.h>
 
